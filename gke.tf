1c4c51fe1a762d799f6ece7db0d9bf0ab1a353ef
ptangudu <pradeepkumartangudu@gmail.com>
2025-06-07 11:48:59 +0530

    changes to new version

--- gke.tf
+++ gke.tf
@@ -48,7 +48,7 @@ resource "google_container_node_pool" "primary_nodes" {
   location   = var.region
   cluster    = google_container_cluster.primary.name
   
-  version = "1.30.6"
+  version = "1.32"
   node_count = var.gke_num_nodes
 
   node_config {
